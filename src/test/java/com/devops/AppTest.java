507d9ff63af1c4a878929073ce2d00f2f5aa8ff4
balugenji <balugenji3@gmail.com>
2026-07-17 15:58:03 +0530

    Update AppTest.java

--- src/test/java/com/devops/AppTest.java
+++ src/test/java/com/devops/AppTest.java
@@ -1,12 +1,32 @@
 package com.devops;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
 
     @Test
     void testApp() {
         assertTrue(true);
+    }
+
+    @Test
+    void testAddition() {
+        assertEquals(10, 5 + 5);
+    }
+
+    @Test
+    void testSubtraction() {
+        assertEquals(5, 10 - 5);
+    }
+
+    @Test
+    void testMultiplication() {
+        assertEquals(20, 4 * 5);
+    }
+
+    @Test
+    void testDivision() {
+        assertEquals(5, 20 / 4);
     }
 }
